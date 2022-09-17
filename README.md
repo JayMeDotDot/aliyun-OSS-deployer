f056bdcacc15be1124b10720e85acb606acd464f
JayMe DotDot <810076929@qq.com>
2022-09-17 15:34:48 +0800

    test

---
+++ README.md
@@ -0,0 +1,27 @@
+# aliyun-oss-deployer
+
+通过 GitHub Action 来配置 阿里云OSS 的工具
+
+## 前提条件
+
+- 已开通阿里云对象存储OSS服务。
+- 已创建RAM用户的AccessKey ID和AccessKey Secret。
+  由于云账号AccessKey拥有所有API的访问权限，建议使用RAM用户的AccessKey。如果部署在服务端，请使用RAM或STS的方式进行API访问或日常运维管控操作；如果部署在客户端，请使用STS方式进行API访问。
+
+## 配置项
+
+OSS(option) 中的各项配置说明（[下表如有问题参考官方文档](https://help.aliyun.com/document_detail/64097.html)）
+
+| 配置项 | 类型 | 描述 |
+| - | - | - |
+| accessKeyId | String | 通过阿里云控制台创建的AccessKey ID。 |
+| accessKeySecret | String | 通过阿里云控制台创建的AccessKey Secret。 |
+| stsToken | String | 使用临时授权方式。更多信息，请参见使用STS进行临时授权。 |
+| bucket | String | 通过控制台或PutBucket创建的Bucket。 |
+| endpoint | String | OSS访问域名。 |
+| region | String | Bucket所在的区域， 默认值为oss-cn-hangzhou。 |
+| internal | Boolean | 是否使用阿里云内网访问，默认值为false。例如通过ECS访问OSS，则设置internal为true，采用internal的endpoint可节省费用。|
+| cname | Boolean | 是否支持上传自定义域名，默认值为false。如果设置cname为true，则endpoint传入自定义域名时，自定义域名需要先和Bucket进行绑定。 |
+| isRequestPay | Boolean | Bucket是否开启请求者付费模式，默认值为false。更多信息，请参见请求者付费模式。 |
+| secure | Boolean | 设置secure为true，则使用HTTPS；设置secure为false，则使用HTTP。更多信息，请参见常见问题。 |
+| timeout | String \| Number | 超时时间，默认值为60000，单位为毫秒。 |
---
+++ index.js
@@ -0,0 +1,39 @@
+const core = require('@actions/core')
+const github = require('@actions/github')
+
+try {
+    // `who-to-greet` input defined in action metadata file
+    const nameToGreet = core.getInput('who-to-greet');
+    console.log(`Hello ${nameToGreet}!`);
+    const time = (new Date()).toTimeString();
+    core.setOutput("time", time);
+    // Get the JSON webhook payload for the event that triggered the workflow
+    const payload = JSON.stringify(github.context.payload, undefined, 2)
+    console.log(`The event payload: ${payload}`);
+  // const accessKeyId = core.getInput('access-key-id')
+  // const accessKeySecret = core.getInput('access-key-secret')
+  // const stsToken = core.getInput('sts-token')
+  // const bucket = core.getInput('bucket')
+  // const endpoint = core.getInput('endpoint')
+  // const region = core.getInput('region')
+  // const internal = core.getBooleanInput('internal')
+  // const cname = core.getBooleanInput('cname')
+  // const isRequestPay = core.getBooleanInput('is-request-pay')
+  // const secure = core.getBooleanInput('secure')
+  // const timeout = core.getInput('timeout')
+
+  // console.log(accessKeyId)
+  // console.log(accessKeySecret)
+  // console.log(stsToken)
+  // console.log(bucket)
+  // console.log(endpoint)
+  // console.log(region)
+  // console.log(internal)
+  // console.log(cname)
+  // console.log(isRequestPay)
+  // console.log(secure)
+  // console.log(timeout)
+
+} catch (err) {
+  core.setFailed(err.message)
+}
